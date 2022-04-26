0c51cbb1358ff61834f9e8acb2607560551922fa
IyobedZ <iyobedzekarias@gmail.com>
2022-04-26 14:54:44 +0000

    perfsqr change to fix infinite loop

--- mpz/nextprime.c
+++ mpz/nextprime.c
@@ -56,10 +56,6 @@ mpz_nextprime (mpz_ptr p, mpz_srcptr n)
   mp_size_t pn;
   mp_bitcnt_t nbits;
   unsigned incr;
-  #if WOOPING
-  mp_limb_t base = WOOPB(p); 
-  #endif
-// gmp_printf("%lu\n", base); 
   TMP_SDECL;
 
   /* First handle tiny numbers */
@@ -115,9 +111,6 @@ mpz_nextprime (mpz_ptr p, mpz_srcptr n)
 		goto next;
 	    }
 
-      #if WOOPING
-      WOOPB(p) = base; 
-      #endif
 	  mpz_add_ui (p, p, difference);
 	  difference = 0;
 
